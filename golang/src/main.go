0dd2ffc347586496fb173ea56a64d346e812cecf
Máron <maron.franca@gmail.com>
2021-09-11 19:23:11 -0300

    mock crud

--- golang/src/main.go
+++ golang/src/main.go
@@ -9,13 +9,8 @@ import (
 	"github.com/maronfranc/subscription-system-products/src/products"
 )
 
-func HandleGetObject(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "Hello world!")
-}
-
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", HandleGetObject)
 	products.Routes(r)
 	http.Handle("/", r)
 	port := "8080"
