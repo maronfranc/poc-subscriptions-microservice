5839ca3bf6d61405c9b3b8460ddde07ec9cdaa10
Máron <maron.franca@gmail.com>
2021-09-13 21:49:24 -0300

    config os path

--- products/src/config/config.go
+++ products/src/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"os"
 	"sync"
 
 	"github.com/ilyakaznacheev/cleanenv"
@@ -11,22 +12,27 @@ var _once sync.Once
 
 type Config struct {
 	Server struct {
-		Port uint `yaml:"host"`
-	}
+		Port uint `yaml:"host" env:"SERVER_PORT" env-default:4000`
+	} `yaml:"server"`
 	Database struct {
-		Host         string `yaml:"host"`
-		Port         string `yaml:"port"`
-		DatabaseName string `yaml:"name"`
-		Username     string `yaml:"user"`
-		Password     string `yaml:"password"`
+		Host         string `yaml:"host" env:"MONGO_DB_HOST" env-default:"localhost"`
+		Port         string `yaml:"port" env:"MONGO_DB_PORT" env-default:"27017"`
+		DatabaseName string `yaml:"name" env:"MONGO_DB_NAME" env-default:"mongo-database"`
+		Username     string `yaml:"user" env:"MONGO_DB_USER" env-default:"user"`
+		Password     string `yaml:"password" env:"MONGO_DB_PASSWORD" env-default:"password"`
 	} `yaml:"database"`
 }
 
+// GetConfig read configuration file
 func GetConfig() *Config {
-	var (
-		configPath = "config.yml"
-		cfg        Config
-	)
+	path, exists := os.LookupEnv("CONFIG_PATH")
+
+	var configPath = "config.yml"
+	if exists {
+		configPath = path
+	}
+
+	var cfg Config
 
 	_once.Do(func() {
 		err := cleanenv.ReadConfig(configPath, &cfg)
@@ -35,6 +41,5 @@ func GetConfig() *Config {
 			panic(err)
 		}
 	})
-
 	return &cfg
 }
